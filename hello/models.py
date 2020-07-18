d2299fe74374d0548376e21fc252963c981bcecf
Yuan Chen <18861720589@163.com>
2020-07-17 22:57:14 -0400

    update index

--- hello/models.py
+++ hello/models.py
@@ -11,3 +11,16 @@ class Tutorial(models.Model):
 
     def __str__(self):
         return self.tutorial_title
+
+class TutorialCategory(models.Model):
+
+    tutorial_category = models.CharField(max_length=200)
+    category_summary = models.CharField(max_length=200)
+    category_slug = models.CharField(max_length=200, default=1)
+
+    class Meta:
+        # Gives the proper plural name for admin
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.tutorial_category

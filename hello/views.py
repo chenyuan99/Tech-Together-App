aa15c1acc593688a1d86a622e79f4ea7f9c91212
Yuan Chen <18861720589@163.com>
2020-07-24 02:19:40 -0400

    update admin

--- hello/views.py
+++ hello/views.py
@@ -32,25 +32,25 @@ def login_request(request):
                   context={"form":form})
 
 def register(request):
-    form = UserCreationForm
-    return render(request = request,
-                  template_name = "main/register.html",
-                  context={"form":form})
-    # if request.method == "POST":
-    #     form = UserCreationForm(request.POST)
-    #     if form.is_valid():
-    #         user = form.save()
-    #         username = form.cleaned_data.get('username')
-    #         login(request, user)
-    #         return redirect("main:homepage")
+    # form = UserCreationForm
+    # return render(request = request,
+    #               template_name = "main/register.html",
+    #               context={"form":form})
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            login(request, user)
+            return redirect("main:homepage")
 
-    #     else:
-    #         for msg in form.error_messages:
-    #             print(form.error_messages[msg])
+        else:
+            for msg in form.error_messages:
+                print(form.error_messages[msg])
 
-    #         return render(request = request,
-    #                       template_name = "main/register.html",
-    #                       context={"form":form})
+            return render(request = request,
+                          template_name = "main/register.html",
+                          context={"form":form})
 
     form = UserCreationForm
     return render(request = request,

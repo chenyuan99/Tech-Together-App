a2f78219a104f7de8541f3c5e57d8c08533435fb
Yuan Chen <18861720589@163.com>
2020-07-27 20:28:57 -0400

    update log in register

--- hello/views.py
+++ hello/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from hello.forms import NewUserForm
-from hello.models import Greeting, Tutorial
+from hello.models import Greeting, Tutorial, TutorialCategory
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+import logging
+
+# This retrieves a Python logging instance (or creates it)
+logger = logging.getLogger(__name__)
 
 # Create your views here.
 def index(request):
@@ -29,16 +35,26 @@ def logout_request(request):
     return redirect("index.html")
 
 def login_request(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}")
+                return redirect('/')
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
     form = AuthenticationForm()
     return render(request = request,
-                  template_name = "main/login.html",
-                  context={"form":form})
+                    template_name = "main/login.html",
+                    context={"form":form})    
 
 def register(request):
-    # form = UserCreationForm
-    # return render(request = request,
-    #               template_name = "main/register.html",
-    #               context={"form":form})
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
